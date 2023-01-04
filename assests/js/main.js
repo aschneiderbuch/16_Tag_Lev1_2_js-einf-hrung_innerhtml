1cd9df9e9ab51bdd4f53e8cfd8681ada7548cf25
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-04 13:11:04 +0100

    documentWrite

---
+++ assests/js/main.js
@@ -0,0 +1,10 @@
+document.getElementById("info").innerHTML =" <h1> Hello World</h1>"
+
+let letH2 = document.getElementById("info").innerHTML += "<h2> How are you?</h2>"
+console.log(letH2)
+
+let letContainer = document.getElementById("container").innerHTML = "<p>start of the element</p>"
+console.log(letContainer)
+
+let letDocumentWrite = document.write("end of the element")
+console.log(letDocumentWrite)
